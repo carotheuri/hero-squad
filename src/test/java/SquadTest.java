b82f2be359c66dd834c92c498d5fe09bb5eb6790
carotheuri <princesstheush2@gmail.com>
2023-08-14 01:05:19 +0300

    confirming get method for squad name is working

--- src/test/java/SquadTest.java
+++ src/test/java/SquadTest.java
@@ -14,36 +14,17 @@ public class SquadTest {
         Squad squad = setupNewSquad();
         Assertions.assertEquals(true, squad instanceof  Squad);
     }
-
-//    @Test
-//    public void validateSquadMaxIsNotExceeded() {
-//        Squad squad1 = new Squad(4,3,"hungro","Eradicate hunger");
-//        int newCount = squad1.getInitial_size()
-//    }
-//    @Test
-//    public void validateHeroExistsInOneSquad() {
-//        Squad squad1 = new Squad(4,3,"hungro","Eradicate hunger");
-//        Squad squad2 = new Squad(4,3,"hungro","Eradicate hunger");
-//        Squad squad3 = new Squad(4,3,"hungro","Eradicate hunger");
-//        Squad squad4 = new Squad(4,3,"hungro","Eradicate hunger");
-//        ArrayList<Squad> squads = Squad.getAll();
-//        Map<String, Integer> nameCount = new HashMap<>();
-//        for (Squad squad : squads) {
-//            nameCount.put(squad.getSquad_name(), nameCount.getOrDefault(squad.getSquad_name(), 0) + 1);
-//        }
-//
-//        for (Map.Entry<String, Integer> entry : nameCount.entrySet()) {
-//            Assertions.assertTrue(entry.getValue() <= 1, "Property 'name' appears more than once: " + entry.getKey());
-//        }
-//    }
     @Test
     public void AllSquadsContainsAllSquads_true() throws Exception {
         Squad squad1 = setupNewSquad();
         Squad squad2 = new Squad(4,3,"hungro","Eradicate hunger");
-        List<Squad> squads = Squad.getAll();
-        Assertions.assertEquals(2, squads.size());
-//        Assertions.assertTrue(Squad.getAll().contains(squad1));
-//        Assertions.assertTrue(Squad.getAll().contains(squad2));
+        Assertions.assertTrue(Squad.getAll().contains(squad1));
+        Assertions.assertTrue(Squad.getAll().contains(squad2));
+    }
+    @Test
+    public void SquadInstantiatesWithSquadName_true() throws Exception {
+        Squad squad = setupNewSquad();
+        Assertions.assertEquals("hungro", squad.getSquad_name());
     }
 
     public Squad setupNewSquad(){
