8fb3862774df142e4d11a256189818a16d83b222
carotheuri <princesstheush2@gmail.com>
2023-08-14 09:41:12 +0300

    creating auto populated squad objects to view on page navigation

---
+++ src/main/java/App.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import static spark.Spark.*;
+
+public class App {
+    public static void main(String[] args) {
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            Squad squad1 = new Squad(4,2,"WarFight","Fight all ongoing war");
+            Squad squad2 = new Squad(10,5,"WarDefense","Defend Citizens and Evacuate them for Safety");
+            Squad squad3 = new Squad(3,3,"Drusco","Fight Drug Addiction and help citizens reform");
+            Squad squad4 = new Squad(2,3,"Hungro","Eradicate hunger by carrying out different initiatives");
+            ArrayList<Squad> projects = Squad.getAll();
+            model.put("projects", projects);
+            model.put("content",request.session().attribute("content"));
+            return new ModelAndView(model, "layout.hbs");
+        }, new HandlebarsTemplateEngine());
+    }
+}
