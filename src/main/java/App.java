d0ee3d607b1be5b5086f5da3ae2a5e1e6ecc5157
carotheuri <princesstheush2@gmail.com>
2023-08-15 14:10:29 +0300

    creating the squad template as an independent function

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,17 +11,13 @@ public class App {
         staticFileLocation("/public");
         get("/", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
-            Squad squad1 = new Squad(4,"WarFight","Fight all ongoing war, when negotiations are not working");
-            Squad squad2 = new Squad(10,"WarDefense","Defend Citizens and Evacuate them for Safety");
-            Squad squad3 = new Squad(3,"Drusco","Fight Drug Addiction and help citizens reform");
-            Squad squad4 = new Squad(1,"Hungro","Eradicate hunger by carrying out different initiatives");
-            ArrayList<Squad> squads = Squad.getAll();
+            ArrayList<Squad> squads = generalSquadsSetUp();
             model.put("squads", squads);
             return new ModelAndView(model, "layout.hbs");
         }, new HandlebarsTemplateEngine());
         get("/heroes/new", (request, response) -> { //URL to display form
             Map<String, Object> model = new HashMap<String, Object>();
-            ArrayList<Squad> squads = Squad.getAll();
+            ArrayList<Squad> squads = generalSquadsSetUp();
             model.put("squads", squads);
             return new ModelAndView(model, "newasset-form.hbs");
         }, new HandlebarsTemplateEngine());
@@ -60,5 +56,13 @@ public class App {
             model.put("heroes", heroes);
             return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
+    }
+    public static ArrayList<Squad> generalSquadsSetUp(){
+        Squad squad1 = new Squad(4,"WarFight","Fight all ongoing war, when negotiations are not working");
+        Squad squad2 = new Squad(10,"WarDefense","Defend Citizens and Evacuate them for Safety");
+        Squad squad3 = new Squad(3,"Drusco","Fight Drug Addiction and help citizens reform");
+        Squad squad4 = new Squad(1,"Hungro","Eradicate hunger by carrying out different initiatives");
+        ArrayList<Squad> squads = Squad.getAll();
+        return squads;
     }
 }
