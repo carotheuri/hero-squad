157229cc1a3bb87e549c57a04556b895b2f7dfa0
carotheuri <princesstheush2@gmail.com>
2023-08-15 14:57:16 +0300

    adding a try catch block

--- src/main/java/Squad.java
+++ src/main/java/Squad.java
@@ -47,6 +47,10 @@ public class Squad {
     }
 
     public static Squad findById(int id){
-        return instances.get(id-1); //why minus 1? See if you can figure it out.
+        try {
+            return instances.get(id - 1);
+        } catch (IndexOutOfBoundsException exception) {
+            return null;
+        }
     }
 }
