5df36d22ce5ed6d4d37fae9c2734206bc04252bc
carotheuri <princesstheush2@gmail.com>
2023-08-15 13:58:54 +0300

    creating a an all heroes routes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -14,7 +14,7 @@ public class App {
             Squad squad1 = new Squad(4,"WarFight","Fight all ongoing war, when negotiations are not working");
             Squad squad2 = new Squad(10,"WarDefense","Defend Citizens and Evacuate them for Safety");
             Squad squad3 = new Squad(3,"Drusco","Fight Drug Addiction and help citizens reform");
-            Squad squad4 = new Squad(2,"Hungro","Eradicate hunger by carrying out different initiatives");
+            Squad squad4 = new Squad(1,"Hungro","Eradicate hunger by carrying out different initiatives");
             ArrayList<Squad> squads = Squad.getAll();
             model.put("squads", squads);
             return new ModelAndView(model, "layout.hbs");
@@ -27,19 +27,38 @@ public class App {
         }, new HandlebarsTemplateEngine());
         post("/heroes/new", (request, response) -> { //URL to make new post on POST route
             ArrayList<Squad> squads = Squad.getAll();
+            ArrayList<Hero> heroes = Hero.getAll();
             Map<String, Object> model = new HashMap<String, Object>();
-            int squad_id = Integer.parseInt(request.queryParams("squad"));
+            int passed_squad_id = Integer.parseInt(request.queryParams("squad"));
             String hero_name = request.queryParams("hero-name");
             String hero_age = request.queryParams("hero-age");
             int hero_power = Integer.parseInt(request.queryParams("hero-powers"));
             int hero_weakness = Integer.parseInt(request.queryParams("hero-weakness"));
-            Hero newHero = new Hero(hero_name, hero_age,hero_power,hero_weakness,squad_id,squads);
+            Hero newHero = new Hero(hero_name, hero_age,hero_power,hero_weakness,passed_squad_id,squads);
             int currentSize = newHero.is_Squad_Max_Exceeded();
-            Squad foundSquad = Squad.findById(squad_id); //use it to find task
-            model.put("heroes", foundSquad);
-            model.put("heroes",currentSize);
-            model.put("heroes",newHero);
+            request.session().attribute("currentSize", currentSize);
+            boolean is_Exceeded = false;
+            for (Squad squad : squads) {
+                int session_size = request.session().attribute("currentSize");
+                if((squad.getId() == passed_squad_id) && (session_size > squad.getMax_size()) ){
+                    is_Exceeded = true;
+                    heroes.remove(newHero);
+                    response.redirect("/");
+                    model.put("heroes",heroes);
+                    model.put("response",is_Exceeded);
+                }
+                else{
+                    model.put("response", is_Exceeded);
+                    model.put("hero",newHero);
+                }
+            }
             return new ModelAndView(model, "success.hbs");
+        }, new HandlebarsTemplateEngine());
+        get("/allheroes", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            ArrayList<Hero> heroes = Hero.getAll();
+            model.put("heroes", heroes);
+            return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
     }
 }
--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -12,11 +12,11 @@ public class Hero {
     private int hero_weakness;
     private static ArrayList<Hero> instances = new ArrayList<>();
 
-    private int squad_id;
+    private int foreign_squad_id;
     private static ArrayList<Squad> squadinstances;
     private boolean is_activated;
     private LocalDateTime createdAt;
-    private int id;
+    private int hero_id;
 
     public Hero(String hero_name, String hero_age, int hero_power, int hero_weakness,int squad_id,ArrayList<Squad> all_squads) {
         this.hero_name = hero_name;
@@ -25,8 +25,8 @@ public class Hero {
         this.hero_weakness = hero_weakness;
         squadinstances = all_squads;
         instances.add(this);
-        this.id = instances.size();
-        this.squad_id  = squad_id;
+        this.hero_id = instances.size();
+        this.foreign_squad_id  = squad_id;
     }
 
     public String getHero_name() {
@@ -44,6 +44,10 @@ public class Hero {
         else{
             return "3rd Line Support";
         }
+    }
+
+    public int getForeign_squad_id() {
+        return foreign_squad_id;
     }
 
     public String getHero_age() {
@@ -71,23 +75,23 @@ public class Hero {
     }
 
     public int getId() {
-        return id;
+        return hero_id;
     }
     public int is_Squad_Max_Exceeded(){
         int currentSize;
         Map<Integer,Integer> squadTracker = new HashMap<Integer, Integer>();
         //Create a hashmap with number of occurrences of an object
         for(Hero hero : instances){
-            if(!squadTracker.containsKey(squad_id)){
-                squadTracker.put(squad_id,1);
+            if(!squadTracker.containsKey(foreign_squad_id)){
+                squadTracker.put(foreign_squad_id,1);
             }
             else{
-                int heroInstance = squadTracker.get(squad_id) + 1;
-                squadTracker.replace(squad_id, heroInstance);
+                int heroInstance = squadTracker.get(foreign_squad_id) + 1;
+                squadTracker.replace(foreign_squad_id, heroInstance);
             }
         }
         try{
-            currentSize = squadTracker.get(squad_id);
+            currentSize = squadTracker.get(foreign_squad_id);
             return currentSize;
         }
         catch (Exception e){
